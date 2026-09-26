658226b9be94cf5ea5c7487a65ee41627290f870
ssambwepdmsacco-alt <ssambwepdmsacco@gmail.com>
2026-09-26 07:42:46 +0300

    Refresh PWA cache after install improvements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ssambwe-pdm-shell-v4";
+const CACHE = "ssambwe-pdm-shell-v5";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./apple-touch-icon.png", "./favicon-32.png", "./favicon-48.png"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
